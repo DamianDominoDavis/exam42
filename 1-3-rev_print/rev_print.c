f6348872290e8484a280fff675c99aca0b9d8e49
Collin Brill <collin.brill@gmail.com>
2018-09-21 02:13:19 -0700

    batch 2

--- 1-3-rev_print/rev_print.c
+++ 1-3-rev_print/rev_print.c
@@ -2,5 +2,16 @@
 
 int	main(int c, char **v)
 {
+	char *ptr;
 
+	if (c == 2)
+	{
+		ptr = v[1];
+		while (*ptr)
+			ptr++;
+		while (--ptr >= v[1])
+			write(1, ptr, 1);
+	}
+	write(1, "\n", 1);
+	return (0);
 }
---
+++ 2-0-inter/inter.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+int main(int c, char **v)
+{
+	char *l;
+	char *s;
+	char *r;
+
+	if (c == 3)
+	{
+		
+	}
+	write(1, "\n", 1);
+	return (0);
+}
