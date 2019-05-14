b0bd4c6b8d33f0ccd3ad45a3f37e0c639a1851be
DamianDominoDavis <cbrill@student.42.us.org>
2019-05-14 07:24:18 -0700

    ok

--- intermediate/0/print_doublon/print_doublon.c
+++ intermediate/0/print_doublon/print_doublon.c
@@ -1,62 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_doubleon.c                                   :+:      :+:    :+:   */
+/*   ok.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*   By: cbrill <cbrill@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/22 21:44:52 by jaleman           #+#    #+#             */
-/*   Updated: 2018/10/22 21:44:53 by jaleman          ###   ########.fr       */
+/*   Created: 2019/05/14 07:05:46 by cbrill            #+#    #+#             */
+/*   Updated: 2019/05/14 07:21:03 by cbrill           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-/*
-** Print to stdout all the numbers that are present in both arrays that are
-** passed as parameter.
-** The numbers are separated by a space, and a new line at the end.
-*/
+void    print_doublon(int *a, int na, int *b, int nb) {
+	int ai, bi, spacing;
 
-void	print_doublon(int *a, int na, int *b, int nb)
-{
-	int	i = 0;
-	int	j = 0;
-	int	total = 0;
-
-	while (na > i && nb > j)
-	{
-		if (a[i] == b[j])
-		{
-			if (total > 0)
-				printf(" ");
-			printf("%i", a[i]);
-			i += 1;
-			j += 1;
-			total += 1;
+	ai = bi = spacing = 0;
+	while (ai < na && bi < nb) {
+		if (a[ai] < b[bi])
+			ai++;
+		else if (a[ai] > b[bi])
+			bi++;
+		else {
+			printf("%s%d", (spacing? " ": ""), a[ai++]);
+			bi++;
+			spacing = 1;
 		}
-		else if (a[i] > b[j])
-			j += 1;
-		else
-			i += 1;
 	}
-	printf("\n");
-	return ;
+	printf("%s", "\n");
 }
 
 /*
-** Main function. Uncomment to test this file!
-*/
+int	main(void) {
+	int a[] = {-5, 2, 10, 15, 50, 70, 100, 200, 300, 1200, 5000},
+		b[] = {2, 4, 5, 6, 7, 10, 40, 70};
 
-/*
-int     main(void)
-{
-	int a[] = { -5,  2, 10, 15, 50, 70, 100, 200, 300, 1200, 5000 };
-	int b[] = { 2,  4,  5,  6,  7, 10,  40,  70 };
-	int na = 11;
-	int nb = 8;
-
-	print_doublon(a, na, b, nb);
-	return (0);
+	print_doublon(a, 11, b, 8);
+	return (0);	
 }
 */
